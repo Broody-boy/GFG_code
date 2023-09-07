e4f0549f502cb69f1218a6a48764f9117cd76459
Arshdeep Singh <111040555+Broody-boy@users.noreply.github.com>
2023-09-08 01:02:30 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Print adjacency list/print-adjacency-list.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+        int t = Integer.parseInt(in.readLine());
+        while (t-- > 0) {
+            String s[] = in.readLine().trim().split(" ");
+            int n = Integer.parseInt(s[0]);
+            int m = Integer.parseInt(s[1]);
+            int edges[][] = new int[m][2];
+            for (int i = 0; i < m; i++) {
+                s = in.readLine().trim().split(" ");
+                edges[i][0] = Integer.parseInt(s[0]);
+                edges[i][1] = Integer.parseInt(s[1]);
+            }
+            Solution ob = new Solution();
+            List<List<Integer>> A = ob.printGraph(n, edges);
+            for (var i : A) {
+                Collections.sort(i);
+                for (int j : i) {
+                    out.print(j + " ");
+                }
+                out.println();
+            }
+        }
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution {
+    public List<List<Integer>> printGraph(int V, int edges[][]) {
+        
+        List<List<Integer>> ret = new ArrayList<>();
+        
+        
+        for(int i = 0; i < V; i++)              //make empty array lists at all indexes
+            ret.add(new ArrayList<>());
+            
+            
+        for(int[] pair : edges){                //undirected graph => (1,3) means at index 1, add 3 and at index 3 add 1
+            ret.get(pair[0]).add(pair[1]);
+            ret.get(pair[1]).add(pair[0]);
+        }
+        
+        return ret;
+    }
+}
