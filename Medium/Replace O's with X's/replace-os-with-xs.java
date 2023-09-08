7f32a511ed49f5e0107fb38b38fb2d9dda84f7b7
Arshdeep Singh <111040555+Broody-boy@users.noreply.github.com>
2023-09-08 23:41:43 +0530

    Updated Solution - GfG to GitHub

--- Medium/Replace O's with X's/replace-os-with-xs.java
+++ Medium/Replace O's with X's/replace-os-with-xs.java
@@ -39,34 +39,25 @@ class GFG{
 
 //User function Template for Java
 
+//refer: https://discuss.geeksforgeeks.org/comment/153060307f0d671d07c806b8e49d8e31
+
 class Solution{
     
-    static int[] dx = {0, 0,-1, 1};           //Order: R, L, U, D
-    static int[] dy = {1,-1, 0, 0};          //Order: R, L, U, D
+    static int[] dx = {-1, 1, 0, 0};           //Order: U, D, R, L
+    static int[] dy = {0, 0, -1, 1};           //Order: U, D, R, L
     
     static char[][] fill(int n, int m, char a[][])
     {
         // code here
     
-        for(int col = 0; col < m; col++)
-        {
-            if(a[0][col] == 'O')
-                setNotClosed(a, 0, col, n, m);
-            
-            if(a[n-1][col] == 'O')
-                setNotClosed(a, n-1, col, n, m);
+        // First, go to all the boundaries and find the discaradble Os.
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(isBoundary(i,j,n,m) && a[i][j] == 'O')
+                    setNotClosed(a, i, j, n, m);            //all the Os in contact with any boundary O are also discardable. Hence, called DFS.
+            }
         }
-        
-        for(int row = 0; row < n; row++)
-        {
-            if(a[row][0] == 'O')
-                setNotClosed(a, row, 0, n, m);
-            
-            if(a[row][m-1] == 'O')
-                setNotClosed(a, row, m-1, n, m);
-        }
-        
-        
+    
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(a[i][j] == 'N')
@@ -80,21 +71,25 @@ class Solution{
         
     }
     
-    static void setNotClosed(char[][] arr, int i, int j, int n, int m){
+    static boolean isBoundary(int i, int j, int n, int m){  //just a simple function called initially in fill()(main function) while traverse the the boundary 
+        return (i==0 || i == n-1 || j == 0 || j == m-1);
+    }
+    
+    static void setNotClosed(char[][] arr, int i, int j, int n, int m){     //makes the current discardable element as 'N' and is called recursively(basically DFS) on its immediate four neighbours(U, D, R, L) to find any adjacent discardable 'O'
         arr[i][j] = 'N';
         
         for(int d = 0; d < 4; d++){
             int ii = i + dx[d];
             int jj = j + dy[d];
             
-            if(isValid(ii,jj,n,m) && arr[ii][jj] == 'O')
+            if(isValid(ii,jj,n,m) && arr[ii][jj] == 'O')                    //isValid checks for valid directional neighbours in case of boundary elements. For rest elements(non boundary elements), it is basically useless as it will return true.
                 setNotClosed(arr, ii,jj,n,m);
         }
         
     }
     
     
-    static boolean isValid(int ii, int jj, int n, int m){
+    static boolean isValid(int ii, int jj, int n, int m){                   //tells whether ii, jj lie inside matrix
         return (ii>0 && ii < n && jj > 0 & jj < m);
         
     }
